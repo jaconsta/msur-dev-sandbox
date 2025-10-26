2ebed6a75595fa0d7d1c4bb48a905e86ce9a7d48
Javier Constain <constainopia@gmail.com>
2025-10-26 18:13:05 +0100

    Add + link with network module

--- main.tf
+++ main.tf
@@ -13,7 +13,18 @@ locals {
 }
 
 # Network
-
+module "aws-network" {
+  source                = "github.com/jaconsta/msur-module-aws-network"
+  env_name              = local.env_name
+  vpc_name              = "ja_msur-VPC"
+  cluster_name          = local.k8s_cluster_name
+  aws_region            = local.aws_region
+  main_vpc_cidr         = "10.0.0.0/16"
+  public_subnet_a_cidr  = "10.0.0.0/16"
+  public_subnet_b_cidr  = "10.0.64.0/18"
+  private_subnet_a_cidr = "10.0.128.0/18"
+  private_subnet_b_cidr = "10.0.192.0/18"
+}
 # EKS
 
 # GitOps
