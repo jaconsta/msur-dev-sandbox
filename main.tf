d4a157f2bf313e113c59876a33ff86562815d3de
Javier Constain <constainopia@gmail.com>
2025-10-26 23:25:04 +0100

    Initial ArgoCD release

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ module "aws-network" {
 module "aws-eks" {
   source = "github.com/jaconsta/msur-module-aws-kubernetes"
 
-  ms_namespace       = "microservices"
-  env_name           = local.env_name
-  aws_region         = local.aws_region
-  cluster_name       = local.k8s_cluster_name
-  vpc_id             = module.aws-network.vpc_id
+  ms_namespace        = "microservices"
+  env_name            = local.env_name
+  aws_region          = local.aws_region
+  cluster_name        = local.k8s_cluster_name
+  vpc_id              = module.aws-network.vpc_id
   cluster_subnets_ids = module.aws-network.subnets_ids
 
-  nodegroup_subnets_ids     = module.aws-network.private-subnets_ids
+  nodegroup_subnets_ids    = module.aws-network.private-subnets_ids
   nodegroup_disk_size      = "20"
   nodegroup_instance_types = ["t3.medium"]
   nodegroup_desired_size   = 1
@@ -45,3 +45,12 @@ module "aws-eks" {
 }
 
 # GitOps
+module "argo-cd-server" {
+  source = "github.com/jaconsta/msur-module-argo-cd"
+
+  kubernetes_cluster_id = module.aws-eks.eks_cluster_id
+  kubernetes_cluster_name = module.aws-eks.eks_cluster_name
+  kubernetes_cluster_cert_data = module.aws-eks.eks_cluster_certificate_data
+  kubernetes_cluster_endpoint = module.aws-eks.eks_cluster_endpoint
+  eks_nodegroup_id = module.aws-eks.eks_cluster_nodegroup_id
+}
